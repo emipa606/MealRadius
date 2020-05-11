5ca4c1bde16f616e79a5844113e1ff5b0738e2d3
Mlie <emipa606@users.noreply.github.com>
2020-05-11 20:58:55 +0200

    Added setting for range, also affects all meals based on MealBase

---
+++ Source/MealRadius/Settings/MealRadius_ModSettings.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace MealRadius.Settings
+{
+    // Token: 0x0200000D RID: 13
+    internal class MealRadius_ModSettings : ModSettings
+    {
+        internal static float MealRadius = 128f;
+
+        public void ChangeDef()
+        {
+            List<ThingDef> list = DefDatabase<ThingDef>.AllDefs.ToList();
+            foreach (ThingDef thingDef in list)
+            {
+                try
+                {
+                    if (thingDef.HasModExtension<IsMealBase>())
+                    {
+                        thingDef.ingestible.chairSearchRadius = MealRadius;
+                    }
+                }
+                catch (System.Exception exception)
+                {
+                    Log.Message($"Failed to set range for {thingDef.defName} " + exception);
+                }
+            }
+        }
+
+        public static void ChangeDefPost()
+        {
+            List<ThingDef> list = DefDatabase<ThingDef>.AllDefs.ToList();
+            foreach (ThingDef thingDef in list)
+            {
+                try
+                {
+                    if (thingDef.HasModExtension<IsMealBase>())
+                    {
+                        thingDef.ingestible.chairSearchRadius = MealRadius;
+                    }
+                }
+                catch (System.Exception exception)
+                {
+                    Log.Message($"Failed to set range for {thingDef.defName} " + exception);
+                }
+            }
+        }
+
+        // Token: 0x06000024 RID: 36 RVA: 0x0000345C File Offset: 0x0000165C
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref MealRadius, "MealRadius", 0.0f, false);
+        }
+
+
+    }
+}
---
+++ Source/MealRadius/Settings/WorldComp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld.Planet;
+using Verse;
+
+namespace MealRadius
+{
+    public class IsMealBase : DefModExtension
+    {
+    }
+
+    // Token: 0x0200000F RID: 15
+    internal class WorldComp : WorldComponent
+    {
+        public WorldComp(World world) : base(world)
+        {
+        }
+
+        public override void FinalizeInit()
+        {
+            base.FinalizeInit();
+            float searchRadius = LoadedModManager.GetMod<MealRadiusMod>().GetSettings<MealRadiusSettings>().searchRadius;
+            List<ThingDef> list = DefDatabase<ThingDef>.AllDefs.ToList();
+            foreach (ThingDef thingDef in list)
+            {
+                try
+                {
+                    if (thingDef.HasModExtension<IsMealBase>())
+                    {
+                        thingDef.ingestible.chairSearchRadius =  searchRadius;
+                    }
+                }
+                catch (System.Exception exception)
+                {
+                    Log.Message($"Failed to set range for {thingDef.defName} " + exception);
+                }
+            }
+            Log.Message("MealRadius - Settings loaded", false);
+        }
+    }
+}
